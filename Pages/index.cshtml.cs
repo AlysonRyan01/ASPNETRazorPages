5e1aef087c9bbd4c831054fbe71d948e16684de7
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2024-12-18 00:37:48 -0300

    index page

---
+++ Pages/index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETRazorPages.Pages;
+
+public class index : PageModel
+{
+    public List<Category> Categories { get; set; } = new();
+    
+    public async Task<IActionResult> OnGet()
+    {
+        await Task.Delay(5000);
+        for (int i = 0; i <= 100; i++)
+        {
+            Categories.Add(new Category(i, $"Categoria {i}", i*18.95m));
+        }
+    }
+    
+    public void OnPost()
+    {
+        
+    }
+}
+
+public record Category(int Id, string Title, decimal Price)
+{
+    
+}
